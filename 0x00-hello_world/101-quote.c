22f9a69aa2eb711a50de06a2debdcae6bafaa156
iammoq <mozytiger6@gmail.com>
2022-12-08 03:05:32 -0800

    my advanced c lang

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - Print a text line
+ * Return:1
+*/
+int main(void)
+{
+	write(2, "and that piece of art is useful" - Dora Korpar, 2015-10-19\n");
+	return (1);
+}
