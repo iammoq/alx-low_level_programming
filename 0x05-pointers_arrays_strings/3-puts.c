e2bed7c897baed07128dbe125a022513bfc3997f
iammoq <mozytiger6@gmail.com>
2022-12-28 04:43:32 -0800

    strings and arrays

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 /**
  * _puts - puts a string followed by a new line
  * @str: pointer value
@@ -10,8 +11,8 @@ void _puts(char *str)
 
 	while (str[i] != '\0')
 	{
-		_putchar(str[i]);
+		putchar(str[i]);
 		i++;
 	}
-	_putchar('\n');
+	putchar('\n');
 }
