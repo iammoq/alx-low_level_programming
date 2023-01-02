48d576ecdf368dc68b737f224a30e43d883af3e2
iammoq <mozytiger6@gmail.com>
2023-01-02 01:22:41 -0800

    pointers

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _memset() -  function fills the first n bytes of the memory
+ * area pointed to by s with the constant byte b
+ * @s: size of pointer
+ * @n: memory to be filled
+ * @b: constant
+ * Return: pointer to the memory area s
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int counter;
+
+	for (counter = 0; counter < n; counter++)
+	{
+		s[counter] = b;
+	}
+	return (s);
+}
---
+++ 0x07-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+Program about more pointers which are double pointers and two dimensional arrays
