cd8c78bcffba27f67d7863ab7bd0dcf50c412789
iammoq <mozytiger6@gmail.com>
2023-01-04 01:52:33 -0800

    recur

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,18 +1,18 @@
 #include "main.h"
 /**
- * sqrt - finds square root of two numbers
+ * square - finds square root of two numbers
  * @a: first number
  * @b: second number
  * Return: Squareroot
  */
-int sqrt(int a, int b)
+int square(int a, int b)
 {
 	if (b * b > a)
 		return (-1);
 	if (b * b == a)
 		return (b);
 	else
-		return (sqrt(a, b + 1));
+		return (square(a, b + 1));
 	return (1);
 }
 /**
@@ -24,5 +24,5 @@ int _sqrt_recursion(int n)
 {
 	if (n == 0)
 		return (0);
-	return (sqrt(n, 1));
+	return (square(n, 1));
 }
