0dd252e71c2e2efed9ab3b8cef991a19978bd53c
iammoq <mozytiger6@gmail.com>
2023-01-09 02:21:57 -0800

    malloc

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,50 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * str_concat - return a pointer that points to a newly allocated space
+ * @s1: first array
+ * @s2: second array
+ * Return: 0
+ */
+char *str_concat(char *s1, char *s2)
+{
+	char *dest;
+	int i, j, len1, len2;
+
+	if (s1 == NULL)
+		s1 = "";
+
+	if (s2 == NULL)
+		s2 = "";
+
+	i = len1 = len2 = 0;
+	while (s1[len1] != '\0')
+	{
+		len1++;
+	}
+	while (s2[len2] != '\0')
+	{
+		len2++;
+	}
+	len2++;
+
+	dest = malloc((len1 + len2) * sizeof(*s1));
+	if (dest == NULL)
+		return (NULL);
+
+	i = 0;
+	while (i < len1)
+	{
+		dest[i] = s1[i];
+		i++;
+	}
+	while (j <= len2)
+	{
+		dest[i] = s2[j];
+		i++;
+		j++;
+	}
+
+	return (dest);
+}
