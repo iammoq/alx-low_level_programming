24234d791af2e4c2c675e66ee2c0389a51379bec
iammoq <mozytiger6@gmail.com>
2023-01-09 06:51:01 -0800

    mollac

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,45 @@
+#include "main.h"
+/**
+ * argstostr - prints args
+ * @ac: width of grid
+ * @av: heigth of grid
+ * Return: args
+ */
+char *argstostr(int ac, char **av)
+{
+	char *s;
+	int count = 0, a = 0, b = 0, c = 0;
+
+	if (ac == 0 || av == NULL)
+	{
+		return (NULL);
+	}
+
+	while (a < ac)
+	{
+		while (av[a][b] != '\0')
+		{
+			count++;
+			b++;
+		}
+		a++;
+	}
+	count = count + ac + 1;
+	s = malloc(sizeof(char) * count);
+	if (s == NULL)
+	{
+		Return (NULL);
+	}
+
+	for (a = 0; a < ac; a++)
+	{
+		for (b = 0; av[a][b] != '\0'; b++)
+		{
+			s[c] = av[a][b];
+			c++;
+		}
+		s[c] = '\n';
+		c++;
+	}
+	return (s);
+}
