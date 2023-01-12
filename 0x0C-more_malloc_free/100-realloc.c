7d1ff8ecf9c4a4d7349fd0925019e60bbb62a58b
iammoq <mozytiger6@gmail.com>
2023-01-12 01:36:24 -0800

    mo malloc

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -38,7 +38,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 
 	for (i = 0; i < (old_size || i < new_size); i++)
 	{
-		*(relloc + i) = clone[i];
+		*(oldp + i) = p[i];
 	}
 	free(ptr);
 	return (oldp);
