bcb3147f22a5a85a59be86973f3815f5221c74cf
iammoq <mozytiger6@gmail.com>
2023-01-16 03:09:00 -0800

    preprocessor task

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - main function where compilation occurs
+ * description: what program prints
+ * Return: 0
+ */
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
