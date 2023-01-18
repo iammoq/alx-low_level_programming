b589331cc1f0b525181e76d45791f00b92cd1f5c
iammoq <mozytiger6@gmail.com>
2023-01-18 07:11:07 -0800

    advanced function pointers

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,34 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ *  main - check the code prints opcode
+ *  @argc: argument count
+ *  @argv: argument vector
+ *  Return: 0
+*/
+int main(int argc, char *argv[])
+{
+	char *opc = (char *) main;
+	int i, bytes;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		exit(1);
+	}
+	bytes = atoi(argv[1]);
+
+	if (bytes < 0)
+	{
+		printf("Error\n");
+		exit(2);
+	}
+	for (i = 0; i < nbytes; i++)
+	{
+		printf("%02x", opc[i] & 0xFF);
+		if (i != bytes - 1)
+			printf(" ");
+	}
+	printf("\n");
+	return (0);
+}
